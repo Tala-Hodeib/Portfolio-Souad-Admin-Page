c4bc1369522df3a94172d31fd955b298c50bddf7
Abdullatif-Fk <abdul647.fk@gmail.com>
2020-11-15 15:12:39 +0200

    routage of pages

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -2,25 +2,45 @@ import logo from './logo.svg';
 import Login from "./Login.js";
 import Menu_bar from './components/Menu_bar';
 import './App.css';
+import Nav from './components/Nav';
+import React from 'react';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Experience from './pages/Experience';
+import Skills from './pages/Skills';
+import Contact from './pages/Contact';
+
+
+{/*function App() {
+  return (
+    <div className="App">
+     
+      <Nav/>
+    </div>
+  );
+}
+
+export default App;*/}
+
 
 function App() {
   return (
     <div className="App">
-     {/*} <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>*/}
-      <Menu_bar/>
+      <Router>
+        <Nav />
+        <Switch>
+          <Route path='/home'  component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/experiences' component={Experience} />
+          <Route path='/skills' component={Skills} />
+          <Route path='/contact' component={Contact} />
+
+
+          
+        </Switch>
+      </Router>
     </div>
   );
 }
