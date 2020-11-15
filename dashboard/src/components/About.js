ae7ffc212eeb98dc940badadc2c40e9d9b510c51
Tala-Hodeib <talahodeib@gmail.com>
2020-11-15 19:09:38 +0200

    start about page

---
+++ dashboard/src/components/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {View,Text} from 'react';
+
+
+
+
+const AboutAdminPage = () => (
+    <View>
+        <View>
+            <Text>Edit about me text</Text>
+        </View>
+    </View>
+)
+
+
+
+
+
+
+
+export default AboutAdminPage;
