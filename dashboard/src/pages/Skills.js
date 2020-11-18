841a8bb0851646e456710dd94b98513dc2bd35ee
Tala-Hodeib <talahodeib@gmail.com>
2020-11-18 11:23:42 +0200

    trial

--- dashboard/src/pages/Skills.js
+++ dashboard/src/pages/Skills.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import './Skills.css';
 
 function Skills() {
     return (
         <div className='skills'>
-            <h1>Skills</h1>
+            <h1>Hello World</h1>
         </div>
     )
 }
 
-export default Skills
+export default Skills;
