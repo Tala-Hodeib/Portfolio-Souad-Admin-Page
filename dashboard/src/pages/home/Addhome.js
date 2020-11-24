1ed0bc5f8d5573d8bf0fb00c333eda59989fe3f4
Abdullatif-Fk <abdul647.fk@gmail.com>
2020-11-24 15:20:57 +0200

    create home finished

--- dashboard/src/pages/home/Addhome.js
+++ dashboard/src/pages/home/Addhome.js
@@ -6,34 +6,72 @@ import { Link } from 'react-router-dom';
 
 export default class Addhome extends React.Component {
         createhome = async (e)=>{
-            console.log(e);
-        const title=e.target.title.value;
-        const description=e.target.description.value;
-        const image=e.target.image.value;
-        console.log(title, description, image);
-        //const url=`https://localhost:8000/home/add?title=${title}&description=${description}&image=${image}`;
-        /*try {
+             e.preventDefault();
+             const title=e.target.title.value;
+             const description=e.target.description.value;
+             const image=e.target.image.value;
+             const requestOptions = {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json','Accept':'application/json' },
+                    body: JSON.stringify({ title:title ,description:description,image :image}),
+                
+                };
+                console.log(title, description, image);
+                const url=`http://localhost:8000/home/add`;
+                const response= await fetch(url,requestOptions);  
+                const result= await response.json();
+                console.log(result);
+                e.target.title.value="";
+                e.target.description.value="";
+                e.target.image.value="";
+
+                    
+                
+
+
+
+            //  const requestOptions = {
+            //     method: 'POST',
+            //     headers: { 'Content-Type': 'application/json','Accept':'application/json' },
+            //     body: JSON.stringify({title:title, description:description,image:image})
+            // };
+            // fetch('https://localhost:8000/home/add', requestOptions)
+            //     .then(response => response.json());
+            // const requestOptions = {
+            //     method: 'POST',
+            //     headers: { 'Content-Type': 'application/json','Accept':'application/json' },
+            //     body: JSON.stringify({ title:title ,description:description,image :image})
+            // };
+
             
-        
-        const response= await fetch(url);
-        const result= response.json();
-        console.log(result);
-        } catch (error) {
-           console.log(error);     
-        }*/
+            // //console.log(e)
+            
+            
+            // //console.log(title, description, image);
+    
+            // const url=(`https://localhost:8000/home/add`,requestOptions);
+            // const response= await fetch(url);
+            // const result= await response.json();
+            // console.log(result);
+            //axios.POST(`https://localhost:8000/home/add`,{title:title, description:description,image:image});
+            
+                //.then(data => this.setState({ postId: data.id }));
+           
+       
+       
     }
     render(){
     return (
         <div>
-            <form onSubmit>
+            <form   onSubmit={this.createhome}>
                 
                 <input type="text"  name="title" placeholder="title"/>
             
                 <input type="text" name="description" placeholder="description"/>
                 <input type="text"  name="image" placeholder="image"/>
-                <input type="submit" value="save"/>
+                <input type="submit" value="save" />
                 <Link to="/home">
-                <input type="button" value="cancel"/>
+                <input type="button" value="cancel" />
                 </Link>
             </form>
             
